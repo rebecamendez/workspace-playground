738c84f2325841c8b78d8a13ca1e40030a0d04dd
Rebeca Méndez <rebeca.mendez@getmanfred.com>
2022-05-26 22:38:59 +0200

    feat(server-api): added slow-operation endpoint

--- packages/server-api/src/controllers/index.controller.ts
+++ packages/server-api/src/controllers/index.controller.ts
@@ -8,6 +8,18 @@ class IndexController {
       next(error);
     }
   };
+
+  public slowOperation = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const sleepTime = Number(req.params.seconds || 0) * 1000;
+      await sleep(sleepTime);
+      res.status(200).json({ data: {}, message: 'slowOperation' });
+    } catch (error) {
+      next(error);
+    }
+  };
 }
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export default IndexController;
