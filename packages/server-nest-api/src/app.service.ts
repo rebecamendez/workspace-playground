d116577e814ba2cfc71c4b95b7c8275a33eb50b0
Rebeca Méndez <scullymb@gmail.com>
2023-03-25 22:54:42 +0100

    ci: added git sha

--- packages/server-nest-api/src/app.service.ts
+++ packages/server-nest-api/src/app.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import * as ChildProcess from 'child_process';
 
 @Injectable()
 export class AppService {
   getHello(): { message: string; metadata: object } {
-    const build_version = '1233';
-    const build_sha = '1234';
-    const build_date = '1234';
+    const build_version = process.env.npm_package_version;
+    const build_sha = this.getBuildVersion();
     const current_utc_date = new Date().toUTCString();
 
     return {
       message: `Hello World from: @server-nest-api (v${build_version})`,
-      metadata: { build_version, build_sha, build_date, current_utc_date },
+      metadata: { build_version, build_sha, current_utc_date },
     };
+  }
+
+  private getBuildVersion(): string {
+    let version = '';
+    try {
+      version = ChildProcess.execSync('git rev-parse HEAD').toString().trim();
+    } catch {
+      // do nothing
+    }
+
+    return version;
   }
 }
